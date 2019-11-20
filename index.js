b6666cca5228cfab0d2b1ab267c52cb34230c907
Rony Efendy <ronyefendy@Ronys-MacBook-Air.local>
2019-11-20 09:29:13 +0700

    commit pertama

---
+++ index.js
@@ -0,0 +1,6 @@
+const $ = require('jquery')
+
+$('#mainContainer').load('./pages/main.html')
+$('.sidebar').load('./components/side_nav.html')
+$('.toolbar-header').load('./components/top_toolbars.html')
+$('footer').load('./components/footer.html')
